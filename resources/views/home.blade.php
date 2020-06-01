52fa1cdf78086110ae7ed0a39d0668ba4bd2e42f
sadmiraga <sadmirvela@gmail.com>
2020-06-01 16:45:06 +0200

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+<button type="button" class="btn btn-primary"> Add Premise </button>
+
+<button type="button" class="btn btn-primary"> List of Premise and Punchline </button>
+
+<button type="button" class="btn btn-primary"> Random Premise </button>
+<button type="button" class="btn btn-primary"> Random Premise </button>
+
+@endsection
